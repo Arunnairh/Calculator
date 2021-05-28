fc18d3941b94f2594adadb177539487619532f48
Arun Nair H <hvishnu000@gmail.com>
2021-05-28 20:07:21 +0530

    started giving actions to buttons

--- Calculator/src/Sample.java
+++ Calculator/src/Sample.java
@@ -1,92 +1,130 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class Sample {
+public class Sample implements ActionListener {
 	
+	JFrame jf;
+	JLabel jl;
+	JButton sevenButton;
+	JButton eightButton;
+	JButton nineButton;
+	JButton divButton;
+	JButton clearButton;
+	JButton fourButton;
+	JButton fiveButton;
+	JButton sixButton;
+	JButton multiButton;
+	JButton oneButton;
+	JButton twoButton;
+	JButton threeButton;
+	JButton addButton;
+	JButton dotButton;
+	JButton zeroButton;
+	JButton equalButton;
+	JButton minusButton;
    public Sample() {
 		
 		
-		JFrame jf=new JFrame("Calculator");
+		jf=new JFrame("Calculator");
 		jf.setBounds(600,150,500,500);
 		jf.setLayout(null);
 		
-		JLabel jl=new JLabel("Heyy");
+		jl=new JLabel("Heyy");
 		jl.setBounds(15,10,450,50);
 		jl.setBackground(Color.gray);
 		jl.setOpaque(true);
 		jl.setHorizontalAlignment(SwingConstants.RIGHT);
 		jf.add(jl);
 		
-		JButton sevenButton=new JButton("7");
+		sevenButton=new JButton("7");
 		sevenButton.setBounds(80,80,50,50);  
+		sevenButton.addActionListener(this);
 		jf.add(sevenButton);
 		
-		JButton eightButton=new JButton("8");
-		 eightButton.setBounds(170,80,50,50);  
+	    eightButton=new JButton("8");
+		eightButton.setBounds(170,80,50,50); 
+		eightButton.addActionListener(this);
 		jf.add( eightButton);
 		
-		JButton nineButton=new JButton("9");
+		nineButton=new JButton("9");
 		nineButton.setBounds(260,80,50,50);  
+		nineButton.addActionListener(this);
 		jf.add(nineButton);
 		
-		JButton divButton=new JButton("/");
-		 divButton.setBounds(350,80,50,50);  
+		divButton=new JButton("/");
+		divButton.setBounds(350,80,50,50);
+		divButton.addActionListener(this);
 		jf.add( divButton);
 		
-		JButton clearButton=new JButton("C");
-		clearButton.setBounds(420,80,40,50);  
+		clearButton=new JButton("C");
+		clearButton.setBounds(420,80,40,50); 
+		clearButton.addActionListener(this);
 		jf.add( clearButton);
 		
-		JButton fourButton=new JButton("4");
-		fourButton.setBounds(80,170,50,50);  
+		fourButton=new JButton("4");
+		fourButton.setBounds(80,170,50,50);
+		fourButton.addActionListener(this);
 		jf.add(fourButton);
 		
-		JButton fiveButton=new JButton("5");
-		fiveButton.setBounds(170,170,50,50);  
+		fiveButton=new JButton("5");
+		fiveButton.setBounds(170,170,50,50); 
+		fiveButton.addActionListener(this);
 		jf.add(fiveButton);
 		
-		JButton sixButton=new JButton("6");
-		sixButton.setBounds(260,170,50,50);  
+		sixButton=new JButton("6");
+		sixButton.setBounds(260,170,50,50); 
+		sixButton.addActionListener(this);
 		jf.add(sixButton);
 		
-		JButton multiButton=new JButton("X");
-		multiButton.setBounds(350,170,50,50);  
+		multiButton=new JButton("X");
+		multiButton.setBounds(350,170,50,50); 
+		multiButton.addActionListener(this);
 		jf.add( multiButton);
 		
-		JButton oneButton=new JButton("1");
-		oneButton.setBounds(80,250,50,50);  
+		oneButton=new JButton("1");
+		oneButton.setBounds(80,250,50,50); 
+		oneButton.addActionListener(this);
 		jf.add(oneButton);
 		
-		JButton twoButton=new JButton("2");
-		twoButton.setBounds(170,250,50,50);  
+		twoButton=new JButton("2");
+		twoButton.setBounds(170,250,50,50); 
+		twoButton.addActionListener(this);
 		jf.add(twoButton);
 		
-		JButton threeButton=new JButton("3");
+		threeButton=new JButton("3");
 		threeButton.setBounds(260,250,50,50);  
+		threeButton.addActionListener(this);
 		jf.add(threeButton);
 		
-		JButton addButton=new JButton("+");
-		addButton.setBounds(350,250,50,50);  
+		addButton=new JButton("+");
+		addButton.setBounds(350,250,50,50); 
+		addButton.addActionListener(this);
 		jf.add( addButton);
 		
-		JButton dotButton=new JButton(".");
-		dotButton.setBounds(80,330,50,50);  
+		dotButton=new JButton(".");
+		dotButton.setBounds(80,330,50,50);
+		dotButton.addActionListener(this);
 		jf.add(dotButton);
 		
-		JButton zeroButton=new JButton("0");
-		zeroButton.setBounds(170,330,50,50);  
+		zeroButton=new JButton("0");
+		zeroButton.setBounds(170,330,50,50);
+		zeroButton.addActionListener(this);
 		jf.add(zeroButton);
 		
-		JButton equalButton=new JButton("=");
-		equalButton.setBounds(260,330,50,50);  
+		equalButton=new JButton("=");
+		equalButton.setBounds(260,330,50,50);
+		equalButton.addActionListener(this);
 		jf.add(equalButton);
 		
-		JButton minusButton=new JButton("-");
+		minusButton=new JButton("-");
 		minusButton.setBounds(350,330,50,50);  
+		minusButton.addActionListener(this);
 		jf.add( minusButton);
 		
 		
@@ -102,6 +140,17 @@ public class Sample {
 	
 	public static void main(String[] args) {
 		Sample sf=new Sample();
+		
+	}
+
+
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==sevenButton) {
+			
+		}
+		
 		
 	}
 
